3c779210ea0a7b0f6564a8dff635e070c3371c9d
jongzzz <133956611+jongzzz@users.noreply.github.com>
2024-08-23 16:20:15 +0800

    Configure Firebase

--- expenseSplitting/App.js
+++ expenseSplitting/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./src/config/firebase";
 
 export default function App() {
+  const [expenses, setExpenses] = useState({});
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    const expensesCollection = collection(db, "groups", "JB", "expenses");
+    onSnapshot(expensesCollection, (snapshot) => {
+      const expensesObj = snapshot.docs.reduce((acc, doc) => {
+        acc[doc.id] = doc.data();
+        return acc;
+      }, {});
+      setExpenses(expensesObj);
+    });
+    const usersCollection = collection(db, "groups", "JB", "users");
+    onSnapshot(usersCollection, (snapshot) => {
+      const usersObj = snapshot.docs.reduce((acc, doc) => {
+        acc[doc.id] = doc.data();
+        return acc;
+      }, {});
+      setUsers(usersObj);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        {Object.entries(expenses).map(([id, expense]) => (
+          <View key={id}>
+            <Text>{expense.title}</Text>
+            <Text>{expense.dateTime}</Text>
+            <Text>{expense.amount}</Text>
+            <Text>
+              {users[expense.paidBy]?.firstName}{" "}
+              {users[expense.paidBy]?.lastName}
+            </Text>
+            {expense.participants.map((participantId) => (
+              <View key={participantId}>
+                <Text>
+                  {users[participantId]?.firstName}{" "}
+                  {users[participantId]?.lastName}
+                </Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
